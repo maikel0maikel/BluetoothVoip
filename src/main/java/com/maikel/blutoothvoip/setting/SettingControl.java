1551998a6f0f15499ce037191639e321296ea368
maikel0maikel <maikel@foxmail.com>
2018-03-26 13:40:41 +0800

    更改更新view接口
    更改更新view接口

--- src/main/java/com/maikel/blutoothvoip/setting/SettingControl.java
+++ src/main/java/com/maikel/blutoothvoip/setting/SettingControl.java
@@ -1,7 +1,10 @@
 package com.maikel.blutoothvoip.setting;
 
+import com.maikel.blutoothvoip.bean.BTDevice;
 import com.maikel.blutoothvoip.bluetooth.BluetoothMgr;
 import com.maikel.blutoothvoip.bluetooth.IBluetoothManager;
+import com.maikel.blutoothvoip.bluetooth.observ.BluetoothSubjectManager;
+import com.maikel.blutoothvoip.constant.Constants;
 
 /**
  * Created by maikel on 2018/3/25.
@@ -14,6 +17,7 @@ public class SettingControl implements ISettingController.SettingController {
     private IBluetoothManager bluetoothManager;
 
     public SettingControl(ISettingController.SettingView v) {
+        BluetoothSubjectManager.getInstance().attchBluetoothObserver(this);
         mView = v;
         mView.setPresenter(this);
         bluetoothManager = new BluetoothMgr();
@@ -31,31 +35,40 @@ public class SettingControl implements ISettingController.SettingController {
 
     @Override
     public void notifyOpenState(int openedState) {
-
+        int state = openedState;
+        switch (state){
+            case Constants.BTState.STATE_ON:
+                bluetoothManager.startDiscovery();
+                break;
+        }
+        mView.notifyOpenState(state);
     }
 
     @Override
     public void notifyBoundState(int boundState) {
-
+        int state = boundState;
+        mView.notifyBoundState(state);
     }
 
     @Override
     public void notifyConnectedState(int connectedState) {
-
+        int state = connectedState;
+        mView.notifyBoundState(state);
     }
 
     @Override
     public void notifyDeviceFound(String name, String address) {
-
+        BTDevice btDevice = new BTDevice(name,address);
+        mView.notifyDeviceFound(btDevice);
     }
 
     @Override
     public void notifyScanStarted() {
-
+        mView.notifyScanStarted();
     }
 
     @Override
     public void notifyScanFinished() {
-
+        mView.notifyScanFinished();
     }
 }
