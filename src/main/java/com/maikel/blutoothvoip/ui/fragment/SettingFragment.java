acf64cbc6772588de39b075751982b82b46a61be
maikel0maikel <maikel@foxmail.com>
2018-03-26 13:43:14 +0800

    view显示更新
    view显示更新

--- src/main/java/com/maikel/blutoothvoip/ui/fragment/SettingFragment.java
+++ src/main/java/com/maikel/blutoothvoip/ui/fragment/SettingFragment.java
@@ -13,19 +13,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.maikel.blutoothvoip.BasePresenter;
 import com.maikel.blutoothvoip.R;
 import com.maikel.blutoothvoip.bean.BTDevice;
-import com.maikel.blutoothvoip.bluetooth.BluetoothMgr;
 import com.maikel.blutoothvoip.constant.Constants;
 import com.maikel.blutoothvoip.setting.ISettingController;
 import com.maikel.blutoothvoip.setting.SettingControl;
 import com.maikel.blutoothvoip.ui.adapter.BluetoothScanAdapter;
+import com.maikel.blutoothvoip.widget.DividerItemDecoration;
 import com.maikel.blutoothvoip.widget.SwitchButton;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by maikel on 2018/3/24.
@@ -37,7 +37,8 @@ public class SettingFragment extends Fragment implements ISettingController.Sett
     private SwitchButton mSwitchButton;
     private ProgressBar mProgressBar;
     private Activity mActivity;
-
+    private TextView connect_device_tv;
+    BluetoothScanAdapter adapter;
     private SwitchButton.OnSwitchChangeListener changeListener = new SwitchButton.OnSwitchChangeListener() {
         @Override
         public void onSwitchChange(String name, boolean switched) {
@@ -50,6 +51,13 @@ public class SettingFragment extends Fragment implements ISettingController.Sett
                 setmSwitchButtonState(View.VISIBLE);
                 mController.end();
             }
+        }
+    };
+
+    private BluetoothScanAdapter.OnItemClickListener onItemClickListener = new BluetoothScanAdapter.OnItemClickListener() {
+        @Override
+        public void itemClickListener(BTDevice btDevice) {
+            Toast.makeText(mActivity, ""+btDevice.getBtAddress(), Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -69,7 +77,8 @@ public class SettingFragment extends Fragment implements ISettingController.Sett
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (mRootView == null) {
             mRootView = inflater.inflate(R.layout.fragment_setting, container, false);
-            RecyclerView recyclerView = mRootView.findViewById(R.id.bluetooth_device_rv);
+            connect_device_tv = (TextView) mRootView.findViewById(R.id.connect_device_tv);
+            RecyclerView recyclerView = (RecyclerView) mRootView.findViewById(R.id.bluetooth_device_rv);
             LinearLayoutManager layoutManager = new LinearLayoutManager(mActivity);
             //设置布局管理器
             recyclerView.setLayoutManager(layoutManager);
@@ -77,15 +86,12 @@ public class SettingFragment extends Fragment implements ISettingController.Sett
             layoutManager.setOrientation(OrientationHelper.VERTICAL);
             //设置增加或删除条目的动画
             recyclerView.setItemAnimator(new DefaultItemAnimator());
-            mSwitchButton = mRootView.findViewById(R.id.connect_device_switch);
-            mProgressBar = mRootView.findViewById(R.id.connect_device_progress);
+            recyclerView.addItemDecoration(new DividerItemDecoration(mActivity,DividerItemDecoration.VERTICAL_LIST));
+            mSwitchButton = (SwitchButton) mRootView.findViewById(R.id.connect_device_switch);
+            mProgressBar = (ProgressBar) mRootView.findViewById(R.id.connect_device_progress);
             mSwitchButton.setSwitchChangeListener(changeListener);
-            List<BTDevice> devices = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                BTDevice btDevice = new BTDevice("i=" + i, "12-bc-io-" + i);
-                devices.add(btDevice);
-            }
-            BluetoothScanAdapter adapter = new BluetoothScanAdapter(mActivity, devices);
+            adapter = new BluetoothScanAdapter(mActivity);
+            adapter.setOnItemClickListener(onItemClickListener);
             recyclerView.setAdapter(adapter);
             new SettingControl(this);
         }
@@ -114,33 +120,69 @@ public class SettingFragment extends Fragment implements ISettingController.Sett
         int btState = openedState;
         switch (btState) {
             case Constants.BTState.STATE_TURNING_ON:
+                connect_device_tv.setText(R.string.label_bt_openning);
                 break;
             case Constants.BTState.STATE_ON:
+                connect_device_tv.setText(R.string.lable_bt_opened);
                 break;
             case Constants.BTState.STATE_TURNING_OFF:
+                connect_device_tv.setText(R.string.lable_bt_closing);
                 break;
             case Constants.BTState.STATE_OFF:
+                connect_device_tv.setText(R.string.lable_bt_closed);
                 break;
         }
     }
 
     @Override
     public void notifyBoundState(int boundState) {
-
+        int btState = boundState;
+        switch (btState) {
+            case Constants.BTState.BOND_BONDING:
+                connect_device_tv.setText(R.string.label_bt_bounding);
+                break;
+            case Constants.BTState.BOND_BONDED:
+                connect_device_tv.setText(R.string.lable_bt_bounded);
+                break;
+            case Constants.BTState.BOND_NONE:
+                connect_device_tv.setText(R.string.label_bt_boundnone);
+                break;
+        }
     }
 
     @Override
     public void notifyConnectedState(int connectedState) {
-
+        int btState = connectedState;
+        switch (btState) {
+            case Constants.BTState.STATE_TURNING_ON:
+                connect_device_tv.setText(R.string.label_bt_openning);
+                break;
+            case Constants.BTState.STATE_ON:
+                connect_device_tv.setText(R.string.lable_bt_opened);
+                break;
+            case Constants.BTState.STATE_TURNING_OFF:
+                connect_device_tv.setText(R.string.lable_bt_closing);
+                break;
+            case Constants.BTState.STATE_OFF:
+                connect_device_tv.setText(R.string.lable_bt_closed);
+                break;
+        }
     }
 
     @Override
     public void notifyScanStarted() {
-
+        connect_device_tv.setText(R.string.label_bt_scan);
     }
 
     @Override
     public void notifyScanFinished() {
+        connect_device_tv.setText(R.string.label_bt_scaned);
+        setmProgressBarVisible(View.GONE);
+        setmSwitchButtonState(View.VISIBLE);
+    }
 
+    @Override
+    public void notifyDeviceFound(BTDevice btDevice) {
+        adapter.addDevice(btDevice);
     }
 }
