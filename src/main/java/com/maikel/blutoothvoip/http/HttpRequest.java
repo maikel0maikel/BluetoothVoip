5a5f118797f56c99a55bb1cd45b3bb772da41395
maikel0maikel <maikel@foxmail.com>
2018-03-30 18:16:47 +0800

    Update HttpRequest.java

--- src/main/java/com/maikel/blutoothvoip/http/HttpRequest.java
+++ src/main/java/com/maikel/blutoothvoip/http/HttpRequest.java
@@ -4,32 +4,25 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 
+import java.lang.ref.WeakReference;
+
 /**
- * Created by zWX396902 on 2018/3/30.
+ * Created by maikel on 2018/3/30.
  */
 
-public class HttpRequest {
+public abstract class HttpRequest {
     private static final int HTTP_RESULT = 1;
-    private Handler mHandler = new Handler(Looper.getMainLooper()){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            HttpResponse response = (HttpResponse) msg.obj;
-            if (response == null){
-                response = new HttpResponse("server return null",HttpCodes.HTTP_FAILURE);
-            }
-            httpResponse(response.getResponse(),response.getCode());
-        }
-    };
+    private Handler mHandler = new ResponseHandler(Looper.getMainLooper());
     private ResponseListener listener;
     private boolean isResponseOnMainThread = false;
     private String url;
     private HttpHead mHead;
     private HttpMethod mHttpMethod = HttpMethod.GET;
     IHttpManager httpManager = HttpManagerImpl.getInstance();
-
+    private BaseTask task ;
     public HttpRequest(String url) {
         this.url = url;
+        task = buildTask();
     }
 
     public HttpRequest setHead(HttpHead head) {
@@ -64,8 +57,8 @@ public class HttpRequest {
         return this;
     }
 
-    public ResponseListener getResponseListener() {
-        return listener;
+    public BaseTask getTask(){
+        return task;
     }
 
     public void submitRequest() {
@@ -74,27 +67,52 @@ public class HttpRequest {
 
     public void onResponse(HttpResponse response) {
         HttpResponse tem = response;
-        if (isResponseOnMainThread){
+        if (isResponseOnMainThread) {
             mHandler.obtainMessage(HTTP_RESULT, tem).sendToTarget();
-        }else {
-            httpResponse(tem.getResponse(),tem.getCode());
-        }
-    }
-    private void httpResponse(String response,int code){
-        if (code == HttpCodes.HTTP_OK){
-            if (listener!=null){
-                listener.onSuccess(response);
+        } else {
+            if (tem.getCode() == HttpCodes.HTTP_OK) {
+                if (listener != null) {
+                    listener.onSuccess(response.getResponse());
+                }
+            } else if (tem.getCode() == HttpCodes.HTTP_FAILURE) {
+                listener.onFailure(response.getError());
             }
-        }else if (code == HttpCodes.HTTP_FAILURE){
-            listener.onFailure(response);
         }
     }
 
-   public interface ResponseListener {
-        void onSuccess(String response);
+
+    abstract BaseTask buildTask();
+
+    public interface ResponseListener {
+        void onSuccess(Object response);
 
         void onFailure(String error);
     }
 
+    static class ResponseHandler extends Handler {
+        WeakReference<ResponseListener> listener;
 
+        ResponseHandler(Looper looper) {
+            super(looper);
+        }
+        public void setListener(ResponseListener listener){
+            this.listener = new WeakReference<>(listener);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            HttpResponse response = (HttpResponse) msg.obj;
+            if (response == null) {
+                response = new HttpResponse(null, HttpCodes.HTTP_FAILURE,"server return null");
+            }
+            if (response.getCode() == HttpCodes.HTTP_OK) {
+                if (listener != null) {
+                    listener.get().onSuccess((String )response.getResponse());
+                }
+            } else if (response.getCode() == HttpCodes.HTTP_FAILURE) {
+                if (listener != null) {
+                    listener.get().onFailure(response.getError());
+                }
+            }
+        }
+    }
 }
