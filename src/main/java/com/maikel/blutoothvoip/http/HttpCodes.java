cd60435f1c512ab12421a1b117028c4f47db246a
maikel0maikel <maikel@foxmail.com>
2018-03-30 15:56:47 +0800

    httpcode
    httpcode

---
+++ src/main/java/com/maikel/blutoothvoip/http/HttpCodes.java
@@ -0,0 +1,11 @@
+package com.maikel.blutoothvoip.http;
+
+/**
+ * Created by maikel on 2018/3/30.
+ */
+
+public class HttpCodes {
+    private HttpCodes(){}
+    public static final int HTTP_OK = 1;
+    public static final int HTTP_FAILURE = 0;
+}
