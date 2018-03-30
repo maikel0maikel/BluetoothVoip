0b18d173e5b00124b0c66b12fdc14d85c0eeb423
maikel0maikel <maikel@foxmail.com>
2018-03-30 18:18:28 +0800

    Update HttpResponse.java

--- src/main/java/com/maikel/blutoothvoip/http/HttpResponse.java
+++ src/main/java/com/maikel/blutoothvoip/http/HttpResponse.java
@@ -4,28 +4,38 @@ package com.maikel.blutoothvoip.http;
  * Created by maikel on 2018/3/30.
  */
 
-public class HttpResponse {
-    private String response;
-    private int code;
-    public HttpResponse(){}
-    public HttpResponse(String response, int code) {
-        this.response = response;
+public class HttpResponse<T> {
+    protected int code;
+    protected String error;
+    T response;
+    public HttpResponse(){
+
+    }
+    public HttpResponse(T response, int code, String error){
         this.code = code;
-    }
-
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
+        this.error = error;
         this.response = response;
     }
-
     public int getCode() {
         return code;
     }
 
     public void setCode(int code) {
         this.code = code;
+    }
+
+    public String getError(){
+        return error;
+    }
+    public void setError(String error){
+        this.error = error;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
+    public void setResponse(T response) {
+        this.response = response;
     }
 }
